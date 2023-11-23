53fb42c21413fd11d214330739f01123e086c1c4
DressedHuman <dressed.human@gmail.com>
2023-11-23 14:07:16 +0600

    added simple layouts

---
+++ src/pages/shared/Header/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../../../assets/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt='Header Logo' />
+        </div>
+    );
+};
+
+export default Header;
